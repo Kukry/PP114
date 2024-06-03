4185f7f3b18088622b3105613ae1cc0cbc4fd23e
Molodkin Viktor <vitamin_C2004@yahoo.com>
2024-06-03 23:08:00 +0300

    +Hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,102 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private Util sessionUtil = new Util();
+
     public UserDaoHibernateImpl() {
 
     }
 
-
     @Override
     public void createUsersTable() {
-
+        try {
+            sessionUtil.openTransactionSession();
+            String sql = "CREATE TABLE IF NOT EXISTS USERS (ID BIGINT(255) AUTO_INCREMENT PRIMARY KEY," +
+                    " NAME VARCHAR(255), LASTNAME VARCHAR(255), AGE TINYINT(127) UNSIGNED)";
+            Session session = sessionUtil.getSession();
+            Query query = session.createNativeQuery(sql);
+            query.executeUpdate();
+            sessionUtil.closeTransactionSession();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try {
+            sessionUtil.openTransactionSession();
+            String sql = "DROP TABLE IF EXISTS USERS";
+            Session session = sessionUtil.getSession();
+            Query query = session.createNativeQuery(sql);
+            query.executeUpdate();
+            sessionUtil.closeTransactionSession();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try {
+            sessionUtil.openTransactionSession();
+            Session session = sessionUtil.getSession();
+            session.save(new User(name, lastName, age));
+            sessionUtil.closeTransactionSession();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try {
+            sessionUtil.openTransactionSession();
+            Session session = sessionUtil.getSession();
+            session.delete(session.find(User.class, id));
+            sessionUtil.closeTransactionSession();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> addressList = new ArrayList<>();
+        try {
+            sessionUtil.openTransactionSession();
+            Session session = sessionUtil.getSession();
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<User> criteria = builder.createQuery(User.class);
+            criteria.from(User.class);
+            addressList = session.createQuery(criteria).getResultList();
+            sessionUtil.closeTransactionSession();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+        return addressList;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try {
+            sessionUtil.openTransactionSession();
+            Session session = sessionUtil.getSession();
+            session.createQuery("delete User").executeUpdate();
+            sessionUtil.closeTransactionSession();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,10 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,6 +14,10 @@ public class Util {
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "root1";
 
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+    private Session session;
+    private Transaction transaction;
+
     public Connection getConnection() {
         Connection connection = null;
         try {
@@ -17,5 +26,43 @@ public class Util {
             System.out.println("SQL CONNECTION ERROR");
         }
         return connection;
+    }
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            return new Configuration().configure("hibernate.cfg.xml")
+                    .addAnnotatedClass(jm.task.core.jdbc.model.User.class).buildSessionFactory();
+        } catch (ExceptionInInitializerError e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public Session openTransactionSession() {
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+        }
+        return session;
+    }
+
+    public void closeTransactionSession() {
+        try {
+            transaction.commit();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+        }
     }
 }
